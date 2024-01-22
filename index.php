7110bac49e297cb2c502b590cc2055a4080e9353
anuraj <anurajselvasothy@gmail.com>
2024-01-22 20:18:30 +0530

    Adding Landing Page and that page related images

---
+++ index.php
@@ -0,0 +1,112 @@
+<html>
+	<head>
+		<title>TEACHEASE</title>
+	
+	
+	<link rel="stylesheet" href="index.css">
+	<link rel="icon" type="image/x-icon" href="dashboard/IMG/">
+	</head>
+
+	<body>
+    
+	<ul>
+	<img src = "dashboard/IMG/logo.png" />
+	
+	<li style="float: right"><a href="login.php" class = "btn-log">Login</a></li></button>
+	<!--<li style="float: left">
+		
+			
+		
+	</li>-->
+	
+	<li><a href="#about">About us</a></li>
+	<li><a href="#services">Services</a></li>
+	<li><a href="#home">Home</a></li>
+	</ul>
+	<br/><br/><br/><br/>
+	<br/><br/><br/><br/>
+	
+	<div class="row">
+		<div class = "column">
+			<div class="cont1">
+				<div class="text2">
+					<u>TEACHEASE</u>
+				</div>
+				<div class="text3">
+					Empower Learning
+				</div>
+			</div>
+
+		  <div class="button-container">
+			<a href="signup/studentsignup.php">
+				<button class ="btn1">
+					<span>Student</span>
+				</button>
+			</a>
+			
+			<a a href="signup/teachersignup.php">
+				<button class = "btn1">
+					<span>Teacher</span>
+				</button>
+			</a>
+
+			<a href="signup/parentsignup.php">
+				<button class = "btn1">
+					<span>Parent</span>
+				</button>
+			</a>
+		  </div>
+
+		</div>
+		
+		<div class= "column">
+			<div class="img2">
+			<img src = "dashboard/IMG/main2.jpg" width="400" height="400">
+			</div>
+		</div>
+
+	</div>
+
+	<div id="services">
+        <h2 style="text-align: center;">Services</h2>
+        <div class="service-card">
+            <img src="dashboard/IMG/tutor.jpg" alt="Online Tutors" width="200" height="200">
+            <h3>Online Tutors</h3>
+            <p>Get tutoring from experienced educators to help you excel in your studies.</p>
+        </div>
+        <div class="service-card">
+            <img src="dashboard/IMG/solostudy.jpg" alt="Self Study" width="200" height="200">
+            <h3>Solo Study</h3>
+            <p>Access a wide range of tools and resources to enhance your self-study skills and knowledge.</p>
+        </div>
+        <div class="service-card">
+            <img src="dashboard/IMG/groupstudy.avif" alt="Group Study" width="200" height="200">
+            <h3>Group Study</h3>
+            <p >Collaborate with peers and join study groups to learn together and share knowledge effectively.</p>
+        </div>
+    </div>
+	
+	<div id="about">
+		<h2 style="text-align: center;">About Us</h2>
+		<div class="column">
+			<img src="dashboard/IMG/aboutus.avif" alt="About Us Image" width="400" height="400">
+		</div>
+		<div class="column">
+			<p >
+				<strong>TEACHEASE</strong> is a platform dedicated to empowering learning. Our mission is to provide a conducive and effective learning environment for both students and teachers.
+				We believe that education is the key to a brighter future, and our goal is to make learning more accessible and engaging for everyone.
+			</p>
+		</div>
+		
+	</div>
+
+	<footer>
+        <div class="footer-content">
+            <p>&copy; 2023 TEACHEASE. All rights reserved.</p>
+        </div>
+    </footer>
+
+	
+	
+	</body>
+	</html>
