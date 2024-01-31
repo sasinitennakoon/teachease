97a5f1c63f5d998ec4697e3c9c2f34755a4ea215
Nethma-k <111905294+Nethma-k@users.noreply.github.com>
2024-01-31 10:53:41 +0530

    Update analytics.js

--- Teacher/js/analytics.js
+++ Teacher/js/analytics.js
@@ -1,22 +1,3 @@
-// SIDEBAR TOGGLE
-
-let sidebarOpen = false;
-const sidebar = document.getElementById('sidebar');
-
-function openSidebar() {
-  if (!sidebarOpen) {
-    sidebar.classList.add('sidebar-responsive');
-    sidebarOpen = true;
-  }
-}
-
-function closeSidebar() {
-  if (sidebarOpen) {
-    sidebar.classList.remove('sidebar-responsive');
-    sidebarOpen = false;
-  }
-}
-
 // ---------- CHARTS ----------
 
 // BAR CHART
