8debd2c60c63d84cfe7fa900cf9e71ff98bd4cd9
sasinitennakoon <123575090+sasinitennakoon@users.noreply.github.com>
2024-02-14 15:42:37 +0530

    feedback form

--- Student/Feedback.php
+++ Student/Feedback.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Dashboard</title>
-    <link rel="stylesheet" href="././css/studymaterials.css">
+    <link rel="stylesheet" href="././css/feed.css">
     <link rel="stylesheet" href="././css/dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
@@ -50,3 +50,7 @@
                 </ul>
             </nav>
         </div>
+
+        <div class="content">
+        
+            <h2>Give Your Valueble Feedback Here!</h2>
