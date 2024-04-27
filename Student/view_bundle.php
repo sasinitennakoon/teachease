f80fd2d92a1f13bbc4ab75934fc54bc3df92ff53
sasinitennakoon <123575090+sasinitennakoon@users.noreply.github.com>
2024-04-27 16:51:01 +0530

    flashcard section

---
+++ Student/view_bundle.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Flashcard Bundle</title>
+    <link rel="stylesheet" href="./css/view.css"> 
+</head>
+<body>
+    <button class="dashboard-button" onclick="goBack()">Go to Dashboard</button>
+    <h1>Lets Enjoy!</h1>
+
+    <?php
+    // Include database connection file
+    include '../database/db_con.php';
+
+    
+    ?>
+
+
+    <script>
+    
+        function goBack() {
+            window.history.back();
+        }
+
+        
+    </script>
+</body>
+</html>
